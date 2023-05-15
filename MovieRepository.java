1a4793f90a69ea7e1e4be8cd2e001e33f4dbe919
Nipali <124891630+NipaliBal@users.noreply.github.com>
2023-05-15 20:41:41 +0530

    Delete MovieRepository.java

--- MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.movierating.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.movierating.entity.Movie;
-
-public interface MovieRepository extends JpaRepository<Movie,Long>{
-
-}
